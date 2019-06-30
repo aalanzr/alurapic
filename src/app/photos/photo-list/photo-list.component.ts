539f816c4db3ae6f21bc82bb7f502c39538b4479
Alan <aalanzr@gmail.com>
2019-06-30 15:33:49 -0300

    [ADD] Adicionado novo componente para listar as fotos

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { PhotoService } from '../photo/photo.service';
 import { ActivatedRoute } from '@angular/router';
+import { Photo } from '../photo/photo';
 
 @Component({
   selector: 'app-photo-list',
@@ -10,7 +11,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PhotoListComponent implements OnInit {
 
-  photos: Object[] = [];
+  photos: Photo[] = [];
 
   constructor(
     private photoService: PhotoService,
